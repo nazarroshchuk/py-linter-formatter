24861abe1f6ddb264a1150a8ff4da40406065754
n.roshchuk <n.roshchuk@levi9.com>
2025-09-01 16:50:49 +0200

    fix formatting

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors":[
+        "errors": [
             format_linter_error(error) for error in errors
         ],
         "path": file_path,
